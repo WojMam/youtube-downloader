b75ab278a0bed1b81d27ecdf1870eed69d1a85f6
Wojciech Mamys <woj.mam@gmail.com>
2022-07-29 12:13:14 +0200

    Preparing draft version of ŁuczniczQA videos downloader

--- downloadVideo.py
+++ downloadVideo.py
@@ -1,13 +1,32 @@
 from pytube import YouTube
 from sys import argv
 
-link = argv[1]
-youtubeObject = YouTube(link)
+linksList = [
+"https://www.youtube.com/watch?v=WmsV3_Z6uYY",
+"https://www.youtube.com/watch?v=pLWc6RdrX_E",
+"https://www.youtube.com/watch?v=1GT1AsUfntM",
+"https://www.youtube.com/watch?v=Q6z9ZguY-UQ",
+"https://www.youtube.com/watch?v=9NP_zHE7vO0",
+"https://www.youtube.com/watch?v=0Nk5YLz6FLI",
+"https://www.youtube.com/watch?v=b_jUdtIfjrI",
+"https://www.youtube.com/watch?v=3iZG7azpBr0",
+"https://www.youtube.com/watch?v=c-DmtSrhrvM",
+"https://www.youtube.com/watch?v=edyXP5pAzu8",
+"https://www.youtube.com/watch?v=cBcZz5tv-Cg",
+"https://www.youtube.com/watch?v=eQoCkVlk-bQ",
+"https://www.youtube.com/watch?v=JZ2blZP6DEE"
+]
 
-print("Currently downloading video of: ", youtubeObject.title)
-print("From: ", youtubeObject.author)
-print("From: ", youtubeObject.length)
+# link = argv[1]
 
-audioFromObject = youtubeObject.streams.get_highest_resolution()
+for x in range(len(linksList)):
+    link = linksList[x]
+    youtubeObject = YouTube(link)
 
-audioFromObject.download('./Download/Video')
+    print("Currently downloading video of: ", youtubeObject.title)
+    print("From: ", youtubeObject.author)
+    print("From: ", youtubeObject.length)
+
+    videoFromObject = youtubeObject.streams.get_highest_resolution()
+
+    videoFromObject.download('./Download/Video')
